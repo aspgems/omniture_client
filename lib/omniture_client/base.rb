ee53700f8c1b98d2bbef3425383b5bfc52104f9a
Andrés Cirugeda <andres@aspgems.com>
2011-11-04 09:57:16 +0100

    Allow vars to use other vars instead of a block

--- lib/omniture_client/base.rb
+++ lib/omniture_client/base.rb
@@ -3,7 +3,7 @@ module OmnitureClient
     include OmnitureClient::ClassLevelInheritableAttributes
 
     inheritable_attributes :meta_vars, :js_vars, :js_events
-    
+
     DEFAULT_OPTIONS = { :delimiter => ',',
                         :unique => nil,
                         :expires_in => 0 }
@@ -14,8 +14,8 @@ module OmnitureClient
         options = DEFAULT_OPTIONS.merge(options)
 
         @meta_vars ||= []
-        meta_var = instance_eval("@#{name} ||= OmnitureClient::MetaVar.new('#{name}', '#{options[:delimiter]}', '#{options[:unique]}', #{options[:expires_in]})")
-        meta_var.add_var(block)
+        meta_var = instance_eval("@#{name} ||= OmnitureClient::MetaVar.new(name, options)")
+        meta_var.add_var(block || options[:copy])
         meta_vars << meta_var unless meta_vars.include?(meta_var)
         meta_var
       end
@@ -24,7 +24,7 @@ module OmnitureClient
         @js_vars ||= []
         @js_vars << yield
       end
-      
+
       def event(&block)
         @js_events ||= []
         @js_events << yield
@@ -37,6 +37,10 @@ module OmnitureClient
           end
           @meta_vars = []
         end
+      end
+
+      def find_var(name)
+        meta_vars.find{ |meta| meta.name == name }
       end
 
       def for_action(name, &block)
@@ -63,9 +67,14 @@ module OmnitureClient
     def vars
       meta_vars = self.class.meta_vars || []
       @vars ||= meta_vars.inject([]) do |vars, meta_var|
-        vars << meta_var.value(controller) if meta_var
+        vars << meta_var.value(controller, self) if meta_var
         vars
       end
+    end
+
+    def eval_var(name)
+      meta_var = self.class.find_var(name)
+      meta_var.value(controller, self).value if meta_var
     end
 
     def js_vars
--- lib/omniture_client/meta_var.rb
+++ lib/omniture_client/meta_var.rb
@@ -3,12 +3,12 @@ module OmnitureClient
     attr_reader :name, :delimiter, :cache_key, :expires_in
     attr_accessor :value_procs
 
-    def initialize(name, delimiter, unique, expires_in)
+    def initialize(name, options = {})
       @name = name
       @value_procs = []
-      @delimiter = delimiter
-      @cache_key = "omniture/#{name}/#{unique}"
-      @expires_in = expires_in
+      @delimiter = options[:delimiter]
+      @cache_key = "omniture/#{name}/#{options[:unique]}"
+      @expires_in = options[:expires_in]
     end
 
     def add_var(value_proc)
@@ -16,19 +16,19 @@ module OmnitureClient
     end
 
     # wrap up the value in a Var object and cache if needed
-    def value(scope)
+    def value(scope, reporter)
       if @expires_in > 0
         Rails.cache.fetch(@cache_key, :expires_in => @expires_in) do
-          return_var(scope)
+          return_var(scope, reporter)
         end
       else
-        return_var(scope)
+        return_var(scope, reporter)
       end
     end
-    
-    def return_var(scope)
-      Var.new(name, value_procs.map{ |p| scope.instance_eval(&p) }.flatten.uniq.join(delimiter))
+
+    def return_var(scope, reporter)
+      Var.new(name, value_procs.map{ |p| p.is_a?(Symbol) ? reporter.eval_var(p) : scope.instance_eval(&p) }.flatten.uniq.join(delimiter))
     end
-    
+
   end
 end
